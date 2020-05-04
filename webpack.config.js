03162803977d87d0119b31056e1722f8a43c5d36
kay-ws <50926527+kay-ws@users.noreply.github.com>
2020-05-04 13:39:56 +0900

    Clean up project

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   mode: 'development',
@@ -22,15 +21,6 @@ module.exports = {
             loader: 'ts-loader',
           }
 
-        ]
-      },
-      {
-        test: /\.vue/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'vue-loader',
-          }
         ]
       },
       {
@@ -73,6 +63,7 @@ module.exports = {
             options: {
               esModule: false,
               name: 'images/[name].[ext]',
+              publicPath: '/',
             },
           },
           {
@@ -103,7 +94,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       filename: 'stylesheets/main.css',
     }),
